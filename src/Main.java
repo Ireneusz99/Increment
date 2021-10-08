cc9bd43b95611a4884921f2e7d3bec0c06ec6c74
Ireneusz99 <ireneusz_chrzan@o2.pl>
2021-10-09 01:31:28 +0200

    -prefix hot change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int[][] tab = new int[10][3];
+        Random random = new Random();
 
-        for(int i=0;i<tab.length;i++) {
-            for(int j=0;j<tab[i].length;j++) {
-                tab[i][j] = i+1;
-            }
-        }
+        for (int[] row : tab)
+            Arrays.fill(row,random.nextInt(50));
 
         for(int i=0;i< tab.length;i++) {
             for (int j = 0; j < tab[i].length; j++) {
@@ -16,7 +17,7 @@ public class Main {
             System.out.println(" ");
         }
 
-
+    System.out.println("BYE BRO!");
 
     }
 }
